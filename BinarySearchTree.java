922cf320532b6c478380fbe85358e8d3e497a7c7
Lucía Caballero Ruiz <luciacaballeror@gmail.com>
2022-12-15 16:49:58 +0100

    segundo commit

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,4 +1,24 @@
     public class BinarySearchTree {
+        // represent the node of binary tree
+        public static class Node {
+            int key;
+            Node left, right;
+     
+            public Node(int item) {
+                // assign item to the new node, set left and rigth children to null
+                key = item;
+                left = right = null;
+            }
+        }
+
+        // represent the root of binary tree
+        public Node root;
+
+        // BinarySearchTree constructor
+        public BinarySearchTree() {
+            root = null;
+        }
+
         public static void main(String args[])
         {
            System.out.println("Hola Java...");
